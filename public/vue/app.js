904f00ba92ce899ffd413ba724255bb6708afe69
Vincent Grimmeisen <vincegrixc@gmail.com>
2019-01-17 23:32:47 +0100

    Design finished and cell selection

--- public/vue/app.js
+++ public/vue/app.js
@@ -1,3 +1,4 @@
+var DEBUG=true
 $(document).ready(function () {
   const app = new Vue({
     el: '#app',
@@ -7,7 +8,27 @@ $(document).ready(function () {
         }
     },
     methods: {
-
+      cellSelected: function (event) {
+        // Catch the cases where a Value is in the div element
+        if (event.target.getAttribute("class") == "cell_white") {
+          var cell = {
+            row: event.target.childNodes[0].getAttribute("row"),
+            col: event.target.childNodes[0].getAttribute("col"),
+            value: event.target.childNodes[0].innerText,
+          }
+        } else {
+          var cell = {
+            // Why the fuck does this work?
+            row: event.target.parentElement.childNodes[0].getAttribute("row"),
+            col: event.target.parentElement.childNodes[0].getAttribute("col"),
+            value: event.target.parentElement.childNodes[0].innerText,
+          }
+        }
+      if(DEBUG) console.log(cell);
+      },
+      dontDoAnything: function (event) {
+        event.preventDefault();
+      }
     },
     mounted() {
       fetch("http://127.0.0.1:9000/api/kakuro/initgame")
@@ -18,49 +39,52 @@ $(document).ready(function () {
       })
     },
     template:`
+
         <div class="kakuro-field">
             <div v-for="cell in game" >
 <!-- Empty Cell -->
               <div v-if="cell.cell.type == '0'">
-                <div class="cell_empty"></div>
-              </div>
-<!-- White Cell -->
-              <div v-else-if="cell.cell.type == '1'">
-                <div class="cell_white"></div>
-              </div>
-<!-- Black Cell -->
-              <div v-else-if="cell.cell.type == '2'">
-                <div class="cell_black">
-<!-- Both sums down & right -->
-                  <div v-if="cell.cell.type == '2'">
-                    <h3  class=both_right_sum>
-                      {{cell.cell.type}}
-                    </h3>
-                    <h3  class=both_down_sum>
-                      {{cell.cell.type}}
-                    </h3>
-
-                  </div>
-<!-- down -->
-                  <div v-if="cell.cell.type == '22'">
-                    <h3  class=down_sum>
-                      {{cell.cell.type}}
-                    </h3>
-<!-- right sum -->
-                    <div v-if="cell.cell.type == '22'">
-                      <h3  class=right_sum>
-                        {{cell.cell.type}}
-                      </h3>
-                  </div>
+                <div class="cell_empty" :row="cell.row" :col="cell.col">
                 </div>
               </div>
+<!-- END -->
+<!-- White Cell -->
+              <div v-else-if="cell.cell.type == '1'">
+                <div class="cell_white" v-on:click="cellSelected">
+                  <h3 class="cell_value" :row="cell.row" :col="cell.col" v-on:click="dontDoAnything">
+                  </h3>
+                </div>
               </div>
-        </div>
+<!-- END -->
+<!-- Black Cell -->
+              <div v-else-if="cell.cell.type == '2'">
+                <div class="cell_black" :row="cell.row" :col="cell.col">
+<!-- Both sums down & right -->
+                  <div v-if="cell.cell.down != '0' && cell.cell.right != '0'">
+                    <h3  class="both_right_sum" >
+                      {{cell.cell.right}}
+                    </h3>
+                    <h3  class="both_down_sum">
+                      {{cell.cell.down}}
+                    </h3>
+                  </div>
+<!-- down -->
+                  <div v-if="cell.cell.down != '0' && cell.cell.right == '0'">
+                    <h3  class=down_sum>
+                      {{cell.cell.down}}
+                    </h3>
+                  </div>
+<!-- right sum -->
+                    <div v-if="cell.cell.right != '0' && cell.cell.down == '0'">
+                      <h3  class=right_sum>
+                        {{cell.cell.right}}
+                      </h3>
+                    </div>
+                  </div>
+                </div>
+<!-- END -->
+                </div>
+              </div>
     `,
   });
-
 });
-
-Vue.component('kakuro-cell', {
-  template: ''
-})
