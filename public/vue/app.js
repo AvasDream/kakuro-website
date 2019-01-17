0a7aea6fccc336531bf1b19b89925250d2d173d3
Vincent Grimmeisen <vincegrixc@gmail.com>
2019-01-17 21:19:20 +0100

    CSS styling for field

--- public/vue/app.js
+++ public/vue/app.js
@@ -20,14 +20,39 @@ $(document).ready(function () {
     template:`
         <div class="kakuro-field">
             <div v-for="cell in game" >
+<!-- Empty Cell -->
               <div v-if="cell.cell.type == '0'">
                 <div class="cell_empty"></div>
               </div>
+<!-- White Cell -->
               <div v-else-if="cell.cell.type == '1'">
                 <div class="cell_white"></div>
               </div>
+<!-- Black Cell -->
               <div v-else-if="cell.cell.type == '2'">
-                <div class="cell_black"></div>
+                <div class="cell_black">
+<!-- Both sums down & right -->
+                  <div v-if="cell.cell.type == '2'">
+                    <h3  class=both_right_sum>
+                      {{cell.cell.type}}
+                    </h3>
+                    <h3  class=both_down_sum>
+                      {{cell.cell.type}}
+                    </h3>
+
+                  </div>
+<!-- down -->
+                  <div v-if="cell.cell.type == '22'">
+                    <h3  class=down_sum>
+                      {{cell.cell.type}}
+                    </h3>
+<!-- right sum -->
+                    <div v-if="cell.cell.type == '22'">
+                      <h3  class=right_sum>
+                        {{cell.cell.type}}
+                      </h3>
+                  </div>
+                </div>
               </div>
               </div>
         </div>
